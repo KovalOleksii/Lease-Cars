a7bedabf3f2a7a380b498f094f83e742ff9eb430
KovalOleksii <128637476+KovalOleksii@users.noreply.github.com>
2023-04-22 15:14:55 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,39 @@
-const swiper = new Swiper(".swiper", {
-  effect: "fade",
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-});
+
+// програмирование табов
+
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabItem.forEach(function(element) {
+  element.addEventListener('click', open);
+})
+
+function open(evt) {
+  const tabTarget = evt.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  tabItem.forEach(function(item){
+    item.classList.remove('tabs__btn-item--activ');
+  })
+
+  tabTarget.classList.add('tabs__btn-item--activ');
+
+  tabContent.forEach(function(item) {
+    item.classList.remove('tabs__content-item__active');
+  })
+
+  document.querySelector(`#${button}`).classList.add('tabs__content-item__active')
+}
+
+// слайдер
+
+  const swiper = new Swiper(".swiper", {
+    effect: "fade",
+    pagination: {
+      el: ".swiper-pagination",
+    },
+   autoplay: {
+     delay: 3000,
+      disableOnInteraction: false,
+    },
+  });
